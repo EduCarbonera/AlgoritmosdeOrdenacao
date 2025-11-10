e7690ceaff3c19e5baf031469e1bd66f589008f1
kaio-333 <dopperkaio@gmail.com>
2025-11-10 12:10:10 -0300

    feat: tests on main

--- src/Main.java
+++ src/Main.java
@@ -22,9 +22,23 @@ public class Main {
             Util.executeTest("testdata/decrescente_10000.csv", SortAlgorithm.BUBBLE_SORT);
             System.out.println(" ");
 
+            System.out.println("--------QUICK SORT--------");
+
             Util.executeTest("testdata/aleatorio_100.csv", SortAlgorithm.QUICK_SORT);
+            Util.executeTest("testdata/aleatorio_1000.csv", SortAlgorithm.QUICK_SORT);
+            Util.executeTest("testdata/aleatorio_10000.csv", SortAlgorithm.QUICK_SORT);
+            System.out.println(" ");
+
+            Util.executeTest("testdata/crescente_100.csv", SortAlgorithm.QUICK_SORT);
             Util.executeTest("testdata/crescente_1000.csv", SortAlgorithm.QUICK_SORT);
+            Util.executeTest("testdata/crescente_10000.csv", SortAlgorithm.QUICK_SORT);
+            System.out.println(" ");
+
             Util.executeTest("testdata/decrescente_100.csv", SortAlgorithm.QUICK_SORT);
+            Util.executeTest("testdata/decrescente_1000.csv", SortAlgorithm.QUICK_SORT);
+            Util.executeTest("testdata/decrescente_10000.csv", SortAlgorithm.QUICK_SORT);
+            System.out.println(" ");
+
 
         } catch (IOException e) {
             System.out.println("An error occurred while reading the file: " + e.getMessage());

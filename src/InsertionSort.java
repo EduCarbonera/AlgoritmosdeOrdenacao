350a291e630876a788e120af4b60dcd58e7369f4
Eduardo Mendes <eduardo.carbonera@pucpr.edu.br>
2025-11-12 19:51:59 -0800

    Add insertion sort implementation
    Implement insertion sort algorithm for sorting an array.

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,2 +1,15 @@
 public class InsertionSort {
+
+    public static void insertionSort(int[] values) {
+        // Segue o principio que o primeiro elemento já está ordenado. Começando do segundo
+        for (int i = 1; i < values.length; i++) {
+            int j = i;
+
+            // Move para a esquerda, trocando com elementos maiores até encontrar a posição correta
+            while (j > 0 && values[j] < values[j - 1]) {
+                Util.swap(values, j, j - 1);
+                j--;  // Continua verificando para a esquerda
+            }
+        }
+    }
 }

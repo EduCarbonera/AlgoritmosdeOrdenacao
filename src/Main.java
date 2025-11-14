e0e6ec5e96cd045208e9e48b842a3422d97bfb5e
kaio-333 <dopperkaio@gmail.com>
2025-11-13 23:10:05 -0300

    feat: better tests on main

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,76 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         try {
 
-            System.out.println("\nBubble Sort Tests:");
+            System.out.println("Sistema de Testes");
 
-            Util.executeTest("testdata/aleatorio_100.csv", SortAlgorithm.BUBBLE_SORT);
-            Util.executeTest("testdata/aleatorio_1000.csv", SortAlgorithm.BUBBLE_SORT);
-            Util.executeTest("testdata/aleatorio_10000.csv", SortAlgorithm.BUBBLE_SORT);
-            System.out.println(" ");
+            System.out.println("Escolha o algoritmo de ordenação:");
+            System.out.println("1 - Bubble Sort");
+            System.out.println("2 - Quick Sort");
+            System.out.println("3 - Insertion Sort");
+            System.out.println("4 - Testar todos os algoritmos");
+            System.out.print("\nDigite sua escolha (1-4): ");
+            int escolha = scanner.nextInt();
+            System.out.println();
 
-            Util.executeTest("testdata/crescente_100.csv", SortAlgorithm.BUBBLE_SORT);
-            Util.executeTest("testdata/crescente_1000.csv", SortAlgorithm.BUBBLE_SORT);
-            Util.executeTest("testdata/crescente_10000.csv", SortAlgorithm.BUBBLE_SORT);
-            System.out.println(" ");
+            switch (escolha) {
+                case 1:
+                    executarTestes(SortAlgorithm.BUBBLE_SORT, "Bubble Sort");
+                    break;
 
-            Util.executeTest("testdata/decrescente_100.csv", SortAlgorithm.BUBBLE_SORT);
-            Util.executeTest("testdata/decrescente_1000.csv", SortAlgorithm.BUBBLE_SORT);
-            Util.executeTest("testdata/decrescente_10000.csv", SortAlgorithm.BUBBLE_SORT);
-            System.out.println(" ");
+                case 2:
+                    executarTestes(SortAlgorithm.QUICK_SORT, "Quick Sort");
+                    break;
 
-            System.out.println("\nQuick Sort Tests:");
+                case 3:
+                    executarTestes(SortAlgorithm.INSERTION_SORT, "Insertion Sort");
+                    break;
 
-            Util.executeTest("testdata/aleatorio_100.csv", SortAlgorithm.QUICK_SORT);
-            Util.executeTest("testdata/aleatorio_1000.csv", SortAlgorithm.QUICK_SORT);
-            Util.executeTest("testdata/aleatorio_10000.csv", SortAlgorithm.QUICK_SORT);
-            System.out.println(" ");
+                case 4:
+                    executarTestes(SortAlgorithm.BUBBLE_SORT, "Bubble Sort");
+                    executarTestes(SortAlgorithm.QUICK_SORT, "Quick Sort");
+                    executarTestes(SortAlgorithm.INSERTION_SORT, "Insertion Sort");
+                    break;
 
-            Util.executeTest("testdata/crescente_100.csv", SortAlgorithm.QUICK_SORT);
-            Util.executeTest("testdata/crescente_1000.csv", SortAlgorithm.QUICK_SORT);
-            Util.executeTest("testdata/crescente_10000.csv", SortAlgorithm.QUICK_SORT);
-            System.out.println(" ");
-
-            Util.executeTest("testdata/decrescente_100.csv", SortAlgorithm.QUICK_SORT);
-            Util.executeTest("testdata/decrescente_1000.csv", SortAlgorithm.QUICK_SORT);
-            Util.executeTest("testdata/decrescente_10000.csv", SortAlgorithm.QUICK_SORT);
-            System.out.println(" ");
-
-            System.out.println("\nInsertion Sort Tests:");
-
-            Util.executeTest("testdata/aleatorio_100.csv", SortAlgorithm.INSERTION_SORT);
-            Util.executeTest("testdata/aleatorio_1000.csv", SortAlgorithm.INSERTION_SORT);
-            Util.executeTest("testdata/aleatorio_10000.csv", SortAlgorithm.INSERTION_SORT);
-            System.out.println(" ");
-
-            Util.executeTest("testdata/crescente_100.csv", SortAlgorithm.INSERTION_SORT);
-            Util.executeTest("testdata/crescente_1000.csv", SortAlgorithm.INSERTION_SORT);
-            Util.executeTest("testdata/crescente_10000.csv", SortAlgorithm.INSERTION_SORT);
-            System.out.println(" ");
-
-            Util.executeTest("testdata/decrescente_100.csv", SortAlgorithm.INSERTION_SORT);
-            Util.executeTest("testdata/decrescente_1000.csv", SortAlgorithm.INSERTION_SORT);
-            Util.executeTest("testdata/decrescente_10000.csv", SortAlgorithm.INSERTION_SORT);
-            System.out.println(" ");
-
+                default:
+                    System.out.println("Opção inválida!");
+                    return;
+            }
 
         } catch (IOException e) {
-            System.out.println("An error occurred while reading the file: " + e.getMessage());
+            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Entrada inválida! Por favor, digite um número.");
+        } finally {
+            scanner.close();
         }
+    }
+
+    private static void executarTestes(SortAlgorithm algoritmo, String nomeAlgoritmo) throws IOException {
+        System.out.println("\n" + nomeAlgoritmo + " Tests:");
+        System.out.println("=".repeat(50));
+
+
+        System.out.println("\n[Dados Aleatórios]");
+        Util.executeTest("testdata/aleatorio_100.csv", algoritmo);
+        Util.executeTest("testdata/aleatorio_1000.csv", algoritmo);
+        Util.executeTest("testdata/aleatorio_10000.csv", algoritmo);
+
+        System.out.println("\n[Dados Crescentes]");
+        Util.executeTest("testdata/crescente_100.csv", algoritmo);
+        Util.executeTest("testdata/crescente_1000.csv", algoritmo);
+        Util.executeTest("testdata/crescente_10000.csv", algoritmo);
+
+        System.out.println("\n[Dados Decrescentes]");
+        Util.executeTest("testdata/decrescente_100.csv", algoritmo);
+        Util.executeTest("testdata/decrescente_1000.csv", algoritmo);
+        Util.executeTest("testdata/decrescente_10000.csv", algoritmo);
+
+        System.out.println();
     }
 }
